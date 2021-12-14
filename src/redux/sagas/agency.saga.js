0c720281965e789b710d28b6622658621600f1d1
Katherine Daugherty <katherinedaugherty23@gmail.com>
2021-12-14 13:50:18 -0600

    agency saga made. root saga hooked up

---
+++ src/redux/sagas/agency.saga.js
@@ -0,0 +1,19 @@
+import { axios } from 'axios';
+import { put, takeLatest } from 'redux-saga/effects';
+
+//Create function to POST new agency to db
+function* postNewAgency(action){
+    try{
+        yield axios.post ('/api/agency/new', action.payload);
+    }
+    catch(error){
+        console.log('error in post new agency', error);       
+        yield put({ type: 'POST_AGENCY_ERROR'}); 
+    }
+}
+
+function* agencySaga(){
+    yield takeLatest('ADD_NEW_AGENCY', postNewAgency);
+}
+
+export default agencySaga;
