bd2939cf010882098f71aafb0d03afa1b1d6733d
Ronald van der Bergh <rhvdbergh@gmail.com>
2021-12-14 14:38:51 -0600

    removes React import, imports specific elements

--- src/components/AgencyRegistration/AgencyRegistration.jsx
+++ src/components/AgencyRegistration/AgencyRegistration.jsx
@@ -1,114 +1,110 @@
-import React from "react";
 import Box from '@mui/material/Box';
 import Stepper from '@mui/material/Stepper';
 import Step from '@mui/material/Step';
 import StepLabel from '@mui/material/StepLabel';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import AgencyRegistrationForm1 from "../AgencyRegistrationForm1/AgencyRegistrationForm1";
-import AgencyRegistrationForm2 from "../AgencyRegistrationForm2/AgencyRegistrationForm2"
-import AgencyRegistrationForm3 from "../AgencyRegistrationForm3/AgencyRegistrationForm3"
-import AgencyRegistrationForm4 from "../AgencyRegistrationForm4/AgencyRegistrationForm4";
+import { useState, Fragment } from 'react';
+import AgencyRegistrationForm1 from '../AgencyRegistrationForm1/AgencyRegistrationForm1';
+import AgencyRegistrationForm2 from '../AgencyRegistrationForm2/AgencyRegistrationForm2';
+import AgencyRegistrationForm3 from '../AgencyRegistrationForm3/AgencyRegistrationForm3';
+import AgencyRegistrationForm4 from '../AgencyRegistrationForm4/AgencyRegistrationForm4';
 
 function AgencyRegistration() {
+  const steps = ['Step 1', 'Step 2', 'Step 3', 'Step 4'];
 
-    const steps = ['Step 1', 'Step 2', 'Step 3', 'Step 4'];
+  const [activeStep, setActiveStep] = useState(0);
 
-    const [activeStep, setActiveStep] = React.useState(0);
+  const [skipped, setSkipped] = useState(new Set());
 
-    const [skipped, setSkipped] = React.useState(new Set());
+  const isStepSkipped = (step) => {
+    return skipped.has(step);
+  };
 
-    const isStepSkipped = (step) => {
-        return skipped.has(step);
-    };
-
-    const handleNext = () => {
-        let newSkipped = skipped;
-        if (isStepSkipped(activeStep)) {
-            newSkipped = new Set(newSkipped.values());
-            newSkipped.delete(activeStep);
-        }
-
-        setActiveStep((prevActiveStep) => prevActiveStep + 1);
-        setSkipped(newSkipped);
-    };
-
-    const handleBack = () => {
-        setActiveStep((prevActiveStep) => prevActiveStep - 1);
-    };
-
-    const handleReset = () => {
-        setActiveStep(0);
-    };
-
-    const handleRender = () => {
-        switch (activeStep){
-            case 0:
-            return <AgencyRegistrationForm1 />
-            case 1:
-            return <AgencyRegistrationForm2 />
-            case 2:
-            return <AgencyRegistrationForm3 />
-            case 3:
-            return <AgencyRegistrationForm4 />
-        }
+  const handleNext = () => {
+    let newSkipped = skipped;
+    if (isStepSkipped(activeStep)) {
+      newSkipped = new Set(newSkipped.values());
+      newSkipped.delete(activeStep);
     }
 
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    setSkipped(newSkipped);
+  };
 
-    return (
-        <>
-            <h1> Agency Registration Page </h1>
-            <p> Will add more over time. Placeholder for now  </p>
-            <Box sx={{ width: '100%' }}>
-                <Stepper activeStep={activeStep}>
-                    {steps.map((label, index) => {
-                        const stepProps = {};
-                        const labelProps = {};
-                        if (isStepSkipped(index)) {
-                            stepProps.completed = false;
-                        }
-                        return (
-                            <Step key={label} {...stepProps}>
-                                <StepLabel {...labelProps}>{label}</StepLabel>
-                            </Step>
-                        );
-                    })}
-                </Stepper>
-                {activeStep === steps.length ? (
-                    <React.Fragment>
-                        <Typography sx={{ mt: 2, mb: 1 }}>
-                            All steps completed - you&apos;re finished
-                        </Typography>
-                        <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
-                            <Box sx={{ flex: '1 1 auto' }} />
-                            <Button onClick={handleReset}>Reset</Button>
-                        </Box>
-                    </React.Fragment>
-                ) : (
-                    <React.Fragment>
-                        {/* handleRender conditionally renders the form */}
-                        {handleRender()}
-                        <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
-                            <Button
-                                color="inherit"
-                                disabled={activeStep === 0}
-                                onClick={handleBack}
-                                sx={{ mr: 1 }}
-                            >
-                                Back
-                            </Button>
-                            <Box sx={{ flex: '1 1 auto' }} />
-                            <Button onClick={handleNext}>
-                                {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
-                            </Button>
-                        </Box>
-                    </React.Fragment>
-                )}
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleReset = () => {
+    setActiveStep(0);
+  };
+
+  const handleRender = () => {
+    switch (activeStep) {
+      case 0:
+        return <AgencyRegistrationForm1 />;
+      case 1:
+        return <AgencyRegistrationForm2 />;
+      case 2:
+        return <AgencyRegistrationForm3 />;
+      case 3:
+        return <AgencyRegistrationForm4 />;
+    }
+  };
+
+  return (
+    <>
+      <h1> Agency Registration Page </h1>
+      <p> Will add more over time. Placeholder for now </p>
+      <Box sx={{ width: '100%' }}>
+        <Stepper activeStep={activeStep}>
+          {steps.map((label, index) => {
+            const stepProps = {};
+            const labelProps = {};
+            if (isStepSkipped(index)) {
+              stepProps.completed = false;
+            }
+            return (
+              <Step key={label} {...stepProps}>
+                <StepLabel {...labelProps}>{label}</StepLabel>
+              </Step>
+            );
+          })}
+        </Stepper>
+        {activeStep === steps.length ? (
+          <Fragment>
+            <Typography sx={{ mt: 2, mb: 1 }}>
+              All steps completed - you&apos;re finished
+            </Typography>
+            <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+              <Box sx={{ flex: '1 1 auto' }} />
+              <Button onClick={handleReset}>Reset</Button>
             </Box>
-        </>)
+          </Fragment>
+        ) : (
+          <Fragment>
+            {/* handleRender conditionally renders the form */}
+            {handleRender()}
+            <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+              <Button
+                color="inherit"
+                disabled={activeStep === 0}
+                onClick={handleBack}
+                sx={{ mr: 1 }}
+              >
+                Back
+              </Button>
+              <Box sx={{ flex: '1 1 auto' }} />
+              <Button onClick={handleNext}>
+                {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
+              </Button>
+            </Box>
+          </Fragment>
+        )}
+      </Box>
+    </>
+  );
 }
-
-
-
 
 export default AgencyRegistration;
