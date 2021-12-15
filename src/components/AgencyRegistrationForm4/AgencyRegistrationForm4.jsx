9b4696dcb2062b1da59664b8893ff96669d9cf3d
Matt Dow <matt.dow.vo@gmail.com>
2021-12-15 08:38:51 -0600

    Started regform 4, saving changes

--- src/components/AgencyRegistrationForm4/AgencyRegistrationForm4.jsx
+++ src/components/AgencyRegistrationForm4/AgencyRegistrationForm4.jsx
@@ -5,9 +5,40 @@ import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import FormControl from '@mui/material/FormControl';
 import FormLabel from '@mui/material/FormLabel';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect, useState } from 'react';
 
 
-function AgencyRegistrationForm4() {
+function AgencyRegistrationForm4({ setCanMoveForward }) {
+  const dispatch = useDispatch();
+
+  const agency = useSelector((store) => store.newAgency);
+
+  // define the state variables the component will be altering
+  const [team_size, setTeam_size] = useState(null);  
+  const [minority_owned, setMinority_owned] = useState(null);
+  const [woman_owned, setWoman_owned] = useState(null);
+  const [veteran_owned, setVeteran_owned] = useState(null);
+
+  // on page load, set local state to what has already been entered
+  // in the agency object, if anything
+  useEffect(() => {
+    setTeam_size(agency.team_size);
+    setMinority_owned(agency.minority_owned);
+    setWoman_owned(agency.woman_owned);
+    setVeteran_owned(agency.veteran_owned)
+  }, []);
+
+  // when any required fields change, check to see if we can move forward
+  useEffect(() => {
+    isCompletedForm();
+  }, [team_size, minority_owned, woman_owned, veteran_owned]);
+
+  // validate that required fields in the form are filled out
+  const isCompletedForm = () => {
+
+    
+  }
 
     return (
         <>
