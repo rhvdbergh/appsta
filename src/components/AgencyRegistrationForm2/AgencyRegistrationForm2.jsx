567c6a03346adcab0c8435682a75aa7a94c50dc6
Ronald van der Bergh <rhvdbergh@gmail.com>
2021-12-14 21:25:02 -0600

    adds validation, onblur, and dispatch to registration-form2

--- src/components/AgencyRegistrationForm2/AgencyRegistrationForm2.jsx
+++ src/components/AgencyRegistrationForm2/AgencyRegistrationForm2.jsx
@@ -1,37 +1,117 @@
-import { TextField, Stack } from "@mui/material";
-import Grid from "@mui/material/Grid"
+import { TextField, Stack } from '@mui/material';
+import Grid from '@mui/material/Grid';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect, useState } from 'react';
 
-function AgencyRegistrationForm2() {
+function AgencyRegistrationForm2({ setCanMoveForward }) {
+  const dispatch = useDispatch();
 
-    return (
-        <>
+  const agency = useSelector((store) => store.newAgency);
 
-            {/* <Grid> */}
-            <Stack>
-                <TextField label="Agency Name*" variant="outlined" />
-                <TextField
-                    multiline={true}
-                    rows={3}
-                    sx={{width: .3, height: 100 }}
-                    placeholder='Tell the customers about your company'
-                    type="text"
-                // value
-                />
-                <TextField label="Logo URL" variant="outlined"
-                />
-                <TextField label="Contact First Name*" variant="outlined"
-                />
-                <TextField label="Contact Last Name*" variant="outlined"
-                />
-                <TextField label="Phone Number" variant="outlined"
-                />
+  const [agency_name, setAgency_name] = useState(null);
+  const [agency_blurb, setAgency_blurb] = useState('');
+  const [logo_url, setLogo_url] = useState(null);
+  const [contact_first_name, setContact_first_name] = useState(null);
+  const [contact_last_name, setContact_last_name] = useState(null);
+  const [phone_number, setPhone_number] = useState(null);
 
-            </Stack>
-                
-            {/* </Grid> */}
-        </>
+  // on page load, set the local state to what has already been
+  // entered in the agency object, if that was done previously
+  useEffect(() => {
+    setAgency_name(agency.agency_name);
+    setAgency_blurb(agency.agency_blurb);
+    setLogo_url(agency.logo_url);
+    setContact_first_name(agency.contact_first_name);
+    setContact_last_name(agency.contact_last_name);
+    setPhone_number(agency.phone_number);
+  }, []);
 
-    )
+  // when any of the required fields change, check
+  // to see if we can move forward
+  useEffect(() => {
+    isCompletedForm();
+  }, [agency_name, contact_first_name, contact_last_name]);
+
+  // validate that all required fields in the form are filled out
+  const isCompletedForm = () => {
+    // these three fields are required; only move forward if they
+    // have some content
+    if (
+      agency_name !== null &&
+      agency_name !== '' &&
+      contact_first_name !== null &&
+      contact_first_name !== '' &&
+      contact_last_name &&
+      contact_last_name !== ''
+    ) {
+      setCanMoveForward(true);
+    } else {
+      setCanMoveForward(false);
+    }
+  };
+
+  // add data to the redux store
+  const handleData = (data, value) => {
+    // check to see that the data field is not empty
+    if (data !== '') {
+      dispatch({
+        type: 'SET_NEW_AGENCY',
+        payload: { ...agency, [data]: value },
+      });
+    }
+  };
+
+  return (
+    <>
+      <Stack>
+        <TextField
+          label="Agency Name*"
+          value={agency_name}
+          variant="outlined"
+          onBlur={() => {
+            handleData('agency_name', agency_name);
+          }}
+        />
+        <TextField
+          multiline={true}
+          rows={3}
+          sx={{ width: 0.3, height: 100 }}
+          label="Tell us more about your company*"
+          value={agency_blurb}
+          type="text"
+          onBlur={() => {
+            handleData('agency_blurb', agency_blurb);
+          }}
+          // value
+        />
+        <TextField label="Logo URL" variant="outlined" value={logo_url} />
+        <TextField
+          label="Contact First Name*"
+          variant="outlined"
+          value={contact_first_name}
+          onBlur={() => {
+            handleData('contact_first_name', contact_first_name);
+          }}
+        />
+        <TextField
+          label="Contact Last Name*"
+          variant="outlined"
+          value={contact_last_name}
+          onBlur={() => {
+            handleData('contact_last_name', contact_last_name);
+          }}
+        />
+        <TextField
+          label="Phone Number"
+          variant="outlined"
+          value={phone_number}
+          onBlur={() => {
+            handleData('phone_number', phone_number);
+          }}
+        />
+      </Stack>
+    </>
+  );
 }
 
 export default AgencyRegistrationForm2;
