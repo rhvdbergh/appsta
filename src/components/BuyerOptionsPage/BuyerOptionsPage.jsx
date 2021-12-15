3138a36298cc95d0d50234840ca459ec4019e5c8
Katherine Daugherty <katherinedaugherty23@gmail.com>
2021-12-15 15:36:25 -0600

    saving added useEffect and Dispatch to buyer options page

--- src/components/BuyerOptionsPage/BuyerOptionsPage.jsx
+++ src/components/BuyerOptionsPage/BuyerOptionsPage.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import OptionsList from "../OptionsList/OptionsList";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 
 function BuyerOptionsPage() {
+
+    const dispatch = useDispatch();
+
+    useEffect(() => { 
+        dispatch({type: "GET_FEATURES"})
+    },[]);
 
     return (
         <>
             <h1> Buyer Options Page </h1>
             <p> Will add more over time. Placeholder for now  </p>
-            <OptionsList/>
+            <OptionsList />
         </>)
 }
 
