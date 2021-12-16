f7294bea382992bacbc09ec14ddc295af1a00931
Ronald van der Bergh <rhvdbergh@gmail.com>
2021-12-16 13:42:13 -0600

    adds t-shirt slider bar

--- src/components/AgencyOptionsSelectionBlock/AgencyOptionsSelectionBlock.jsx
+++ src/components/AgencyOptionsSelectionBlock/AgencyOptionsSelectionBlock.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import {
+  Box,
+  Slider,
   Button,
   Typography,
   InputLabel,
@@ -13,7 +15,7 @@ function AgencyOptionsSelectionBlock({ feature }) {
   // else localStorage will set this to null
   // it would have been saved as a string, so make it an object
   const savedFeature = JSON.parse(
-    localStorage.getItem(`feature_${feature.id}`)
+    localStorage.getItem(`agency_feature_${feature.id}`)
   );
   // local state for the quantity Select dropdown
   const [quantity, setQuantity] = useState(
@@ -30,9 +32,12 @@ function AgencyOptionsSelectionBlock({ feature }) {
   const handleQuantity = (event) => {
     setQuantity(event.target.value);
     // if already in localStorage, update
-    if (localStorage.getItem(`feature_${feature.id}`) !== null) {
+    if (localStorage.getItem(`agency_feature_${feature.id}`) !== null) {
       feature.quantity = event.target.value;
-      localStorage.setItem(`feature_${feature.id}`, JSON.stringify(feature));
+      localStorage.setItem(
+        `agency_feature_${feature.id}`,
+        JSON.stringify(feature)
+      );
     }
   };
 
@@ -44,38 +49,57 @@ function AgencyOptionsSelectionBlock({ feature }) {
       feature.quantity = quantity;
       // it has not been added yet, so add it
       // we need to JSON.stringify because localStorage can't store objects
-      localStorage.setItem(`feature_${feature.id}`, JSON.stringify(feature));
+      localStorage.setItem(
+        `agency_feature_${feature.id}`,
+        JSON.stringify(feature)
+      );
     } else {
       // it's already there, so remove it
-      localStorage.removeItem(`feature_${feature.id}`);
+      localStorage.removeItem(`agency_feature_${feature.id}`);
       // reset the quantity to 1
       setQuantity(1);
     }
   };
 
+  // sets the t-shirt size
+  const tShirtSizesMarks = [
+    {
+      value: 0,
+      label: 'Not Offered',
+    },
+    {
+      value: 20,
+      label: 'S',
+    },
+    {
+      value: 40,
+      label: 'M',
+    },
+    {
+      value: 60,
+      label: 'L',
+    },
+    {
+      value: 80,
+      label: 'XL',
+    },
+    {
+      value: 100,
+      label: 'XXL',
+    },
+  ];
+
   return (
     <>
-    <p> HELLO HELLO HELLO </p>
-      <Button variant="contained" onClick={handleClick}>
-        <Typography variant="body1">
-          {addedToProject ? 'Remove from my project' : 'Add to my project'}
-        </Typography>
-      </Button>
-      <FormControl fullWidth>
-        <InputLabel id="quantity">Quantity</InputLabel>
-        <Select
-          labelId="quantity"
-          value={quantity}
-          label="Quantity"
-          onChange={handleQuantity}
-        >
-          <MenuItem value={1}>1</MenuItem>
-          <MenuItem value={2}>2</MenuItem>
-          <MenuItem value={3}>3</MenuItem>
-          <MenuItem value={4}>4</MenuItem>
-          <MenuItem value={5}>5</MenuItem>
-        </Select>
-      </FormControl>
+      <Box sx={{ width: 300 }}>
+        <Slider
+          aria-label="Custom marks"
+          defaultValue={20}
+          step={20}
+          valueLabelDisplay="off"
+          marks={tShirtSizesMarks}
+        />
+      </Box>
     </>
   );
 }
