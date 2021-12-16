4fb8d78d4f6939cd8ed4d12f7c9c26d578e45bf9
Matt Dow <matt.dow.vo@gmail.com>
2021-12-16 15:14:50 -0600

    Added hardcoded cost ranges

--- src/components/BuyerReviewSelection/BuyerReviewSelection.jsx
+++ src/components/BuyerReviewSelection/BuyerReviewSelection.jsx
@@ -3,7 +3,8 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Navbar from '../Navbar/Navbar';
 import Box from '@mui/material/Box';
-import Button from '@mui/material/Button'
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 import { useHistory } from 'react-router-dom';
 import OptionsList from '../OptionsList/OptionsList';
 
@@ -29,6 +30,13 @@ function BuyerReviewSelection() {
         <Box>
           <h1>Review your project</h1>
           <OptionsList features={selectedFeatures} listType={'buyer-review'} />
+          <Typography variant="h6" sx={{ my:1 }}>
+            Cost Range for *Feature* Group: $1,234 - $3,456
+          </Typography>
+          <Typography variant="h6" sx={{ my:1 }}>
+            Total cost range: $4,567 - $6,789
+          </Typography>
+          
           <Button onClick={handleFeatureChange}>Change Features</Button>
           <Button>Register to view quotes</Button>
         </Box>
