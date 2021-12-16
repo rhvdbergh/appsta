c16d6f36bdf14c1923548a1b723576b78418f62d
Ronald van der Bergh <rhvdbergh@gmail.com>
2021-12-15 22:21:02 -0600

    temporary selected features retrieved from localStorage on submit

--- src/components/BuyerOptionsPage/BuyerOptionsPage.jsx
+++ src/components/BuyerOptionsPage/BuyerOptionsPage.jsx
@@ -1,39 +1,55 @@
-import React from "react";
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import OptionsList from "../OptionsList/OptionsList";
+import React from 'react';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import OptionsList from '../OptionsList/OptionsList';
 import Navbar from '../Navbar/Navbar';
 import Box from '@mui/material/Box';
 
 function BuyerOptionsPage() {
-    const selectedCategory = useSelector((store) => store.selectedCategory);
-    const submitQuote = () => {
-        console.log('Quote Submitted');
-    }
+  const selectedCategory = useSelector((store) => store.selectedCategory);
 
-    const dispatch = useDispatch();
-    const features = useSelector(store => store.features);
+  const submitQuote = () => {
+    // build the selectedFeatures object to save in the redux store
+    const selectedFeatures = [];
+    features
+      // filter through features and check if it is in localStorage
+      .filter(
+        (feature) => localStorage.getItem(`feature_${feature.id}`) !== null
+      )
+      // retrieve all those in localStorage and add them to selectedFeatures array
+      // this includes the quantity
+      .forEach((feature) =>
+        selectedFeatures.push(
+          JSON.parse(localStorage.getItem(`feature_${feature.id}`))
+        )
+      );
+    console.log('Quote Submitted', selectedFeatures);
+  };
 
-    useEffect(() => {
-        dispatch({ type: "GET_FEATURES" })
-    }, []);
+  const dispatch = useDispatch();
+  const features = useSelector((store) => store.features);
 
-    return (
+  useEffect(() => {
+    dispatch({ type: 'GET_FEATURES' });
+  }, []);
 
-        <>
-            
-
-            <Box sx={{ display: 'flex' }}>
-                <Navbar btn1text={'SUBMIT QUOTE'} fxn1={submitQuote} />
-                <Box>
-                  <h1> Start Building your Project! </h1>
-                  <p> Select the features to include in your project.  Your estimate will be based on selected features. </p>
-                  < OptionsList features={features} />
-                    <p> Selected Category ID is: {selectedCategory}</p>
-                </Box>
-            </Box>
-        </>)
+  return (
+    <>
+      <Box sx={{ display: 'flex' }}>
+        <Navbar btn1text={'SUBMIT QUOTE'} fxn1={submitQuote} />
+        <Box>
+          <h1> Start Building your Project! </h1>
+          <p>
+            {' '}
+            Select the features to include in your project. Your estimate will
+            be based on selected features.{' '}
+          </p>
+          <OptionsList features={features} />
+          <p> Selected Category ID is: {selectedCategory}</p>
+        </Box>
+      </Box>
+    </>
+  );
 }
-
 
 export default BuyerOptionsPage;
