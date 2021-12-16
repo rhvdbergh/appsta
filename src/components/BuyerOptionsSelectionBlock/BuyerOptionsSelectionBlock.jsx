c27f93dfe12ff51f271463f825d0aab62461dbb6
Ronald van der Bergh <rhvdbergh@gmail.com>
2021-12-15 22:00:14 -0600

    localStorage makes feature data persist

--- src/components/BuyerOptionsSelectionBlock/BuyerOptionsSelectionBlock.jsx
+++ src/components/BuyerOptionsSelectionBlock/BuyerOptionsSelectionBlock.jsx
@@ -8,19 +8,50 @@ import {
   Select,
 } from '@mui/material';
 
-function BuyerOptionsSelectionBlock() {
+function BuyerOptionsSelectionBlock({ feature }) {
+  // grab the feature from the store, if it already exists
+  // else localStorage will set this to null
+  // it would have been saved as a string, so make it an object
+  const savedFeature = JSON.parse(localStorage.getItem(feature.id));
   // local state for the quantity Select dropdown
-  const [quantity, setQuantity] = useState('');
+  const [quantity, setQuantity] = useState(
+    savedFeature === null ? 1 : savedFeature.quantity
+  );
+  // local state to determine whether this has been added to our project
+  // we set this state for each feature according to whether an entry
+  // for that feature exists in localStorage
+  const [addedToProject, setAddedToProject] = useState(
+    savedFeature === null ? false : true
+  );
 
   // handles quantity change
   const handleQuantity = (event) => {
     setQuantity(event.target.value);
   };
 
+  const handleClick = (event) => {
+    setAddedToProject(!addedToProject);
+    // update the localStorage
+    if (savedFeature === null) {
+      // we add the quantity here so we can retrieve it laster
+      feature.quantity = quantity;
+      // it has not been added yet, so add it
+      // we need to JSON.stringify because localStorage can't store objects
+      localStorage.setItem(feature.id, JSON.stringify(feature));
+    } else {
+      // it's already there, so remove it
+      localStorage.removeItem(feature.id);
+      // reset the quantity to 1
+      setQuantity(1);
+    }
+  };
+
   return (
     <>
-      <Button variant="contained">
-        <Typography variant="body1">Add to my project</Typography>
+      <Button variant="contained" onClick={handleClick}>
+        <Typography variant="body1">
+          {addedToProject ? 'Remove from my project' : 'Add to my project'}
+        </Typography>
       </Button>
       <FormControl fullWidth>
         <InputLabel id="quantity">Quantity</InputLabel>
