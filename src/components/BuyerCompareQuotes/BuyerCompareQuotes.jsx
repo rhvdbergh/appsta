e886d2f55cfbd8a0d8c79338838eeeed838a0273
Ronald van der Bergh <rhvdbergh@gmail.com>
2021-12-30 10:28:21 -0600

    adds some dummy data

--- src/components/BuyerCompareQuotes/BuyerCompareQuotes.jsx
+++ src/components/BuyerCompareQuotes/BuyerCompareQuotes.jsx
@@ -1,11 +1,19 @@
 import { Box } from '@mui/material';
 import QuotesCard from '../QuotesCard/QuotesCard';
 
+// dummy data to build quotes object
+// should be replaced with information from db
+const agencyQuote = {
+  agency_name: 'Agency Name',
+  logo_url: 'https://source.unsplash.com/random',
+  agency_blurb: 'put your blurb here',
+};
+
 function BuyerCompareQuotes() {
   return (
     <>
       <Box sx={{ display: 'flex' }}>
-        <QuotesCard />
+        <QuotesCard agencyQuote={agencyQuote} />
       </Box>
     </>
   );
--- src/components/QuotesCard/QuotesCard.jsx
+++ src/components/QuotesCard/QuotesCard.jsx
@@ -1,5 +1,26 @@
-function QuotesCard() {
-  return <p>This is a QuotesCard</p>;
+import {
+  Card,
+  CardHeader,
+  CardActions,
+  Collapse,
+  CardContent,
+  Box,
+  CardMedia,
+  Typography,
+} from '@mui/material';
+
+// expects an agencyQuote object that is similar to the
+// route received from /api/quotes/agencyquote
+function QuotesCard({ agencyQuote }) {
+  return (
+    <Card>
+      <CardHeader title={agencyQuote.agency_name} />
+      <CardMedia component="img" height="200" image={agencyQuote.logo_url} />
+      <CardContent>
+        <Typography>{agencyQuote.agency_blurb}</Typography>
+      </CardContent>
+    </Card>
+  );
 }
 
 export default QuotesCard;
