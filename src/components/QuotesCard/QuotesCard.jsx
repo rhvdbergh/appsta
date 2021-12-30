d558f68282a972c8df73b135a29f50edd8bc2dc0
Ronald van der Bergh <rhvdbergh@gmail.com>
2021-12-30 10:40:01 -0600

    learn more expands info

--- src/components/QuotesCard/QuotesCard.jsx
+++ src/components/QuotesCard/QuotesCard.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import {
   Card,
   CardHeader,
@@ -5,6 +6,7 @@ import {
   Collapse,
   CardContent,
   Box,
+  Button,
   CardMedia,
   Typography,
 } from '@mui/material';
@@ -12,6 +14,9 @@ import {
 // expects an agencyQuote object that is similar to the
 // route received from /api/quotes/agencyquote
 function QuotesCard({ agencyQuote }) {
+  // local state to determine whether card is expanded or not
+  const [isExpanded, setIsExpanded] = useState(false);
+
   return (
     <Card>
       <CardHeader title={agencyQuote.agency_name} />
@@ -19,6 +24,17 @@ function QuotesCard({ agencyQuote }) {
       <CardContent>
         <Typography>{agencyQuote.agency_blurb}</Typography>
       </CardContent>
+      <CardActions>
+        <Button onClick={() => setIsExpanded(!isExpanded)}>
+          {isExpanded ? 'Show Less' : 'Learn More'}
+        </Button>
+      </CardActions>
+      <Collapse in={isExpanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography>my content</Typography>
+          <Typography>my content</Typography>
+        </CardContent>
+      </Collapse>
     </Card>
   );
 }
