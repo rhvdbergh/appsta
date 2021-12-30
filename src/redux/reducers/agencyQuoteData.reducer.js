590cb63f9aa4c5962aa110740a9d9acdc88792c0
Matt Dow <matt.dow.vo@gmail.com>
2021-12-30 13:29:49 -0600

    Adjusted agency quote data reducer

--- src/redux/reducers/agencyQuoteData.reducer.js
+++ src/redux/reducers/agencyQuoteData.reducer.js
@@ -1,7 +1,7 @@
 const agencyQuoteDataReducer = (state = [], action) => {
   switch (action.type) {
     case 'SET_AGENCY_QUOTE_DATA':
-      return [...state, action.payload];
+      return action.payload;
     case 'LOGOUT':
       return [];
     default:
