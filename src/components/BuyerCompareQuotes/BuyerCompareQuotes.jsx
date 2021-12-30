821caa37d6781b514f16cef6a9e694ff15418d63
Ronald van der Bergh <rhvdbergh@gmail.com>
2021-12-30 15:25:35 -0600

    removes dummy data from buyercomparequotes

--- src/components/BuyerCompareQuotes/BuyerCompareQuotes.jsx
+++ src/components/BuyerCompareQuotes/BuyerCompareQuotes.jsx
@@ -3,23 +3,6 @@ import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import QuotesCard from '../QuotesCard/QuotesCard';
 
-// dummy data to build quotes object
-// should be replaced with information from db
-const agencyQuote = {
-  agency_name: 'Agency Name',
-  logo_url: 'https://source.unsplash.com/random',
-  agency_blurb: 'put your blurb here',
-  phone_number: '901-999-9999',
-  city: 'Providence',
-  team_size: '87',
-  minority_owned: true,
-  woman_owned: false,
-  veteran_owned: false,
-  staffing_location: 'Onshore Talent Only',
-  contact_first_name: 'Ridwan',
-  contact_last_name: 'Ali',
-};
-
 // dummy selectedFeatures list
 const selectedFeatures = [
   {
