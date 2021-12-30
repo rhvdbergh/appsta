640071536b065d883cf6389ed9e6acf46b80ed3c
Ronald van der Bergh <rhvdbergh@gmail.com>
2021-12-30 14:55:29 -0600

    retrieves correct agencies for quoting agencies

--- src/components/BuyerCompareQuotes/BuyerCompareQuotes.jsx
+++ src/components/BuyerCompareQuotes/BuyerCompareQuotes.jsx
@@ -1,4 +1,6 @@
 import { Box } from '@mui/material';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import QuotesCard from '../QuotesCard/QuotesCard';
 
 // dummy data to build quotes object
@@ -18,7 +20,41 @@ const agencyQuote = {
   contact_last_name: 'Ali',
 };
 
+// dummy selectedFeatures list
+const selectedFeatures = [
+  {
+    id: 6,
+    feature_name: 'clear table',
+    feature_story: 'this table is clear',
+    feature_description: 'clear table',
+    category_id: 3,
+    image_url: 'https://i.stack.imgur.com/jm4zL.png',
+    quantity: 1,
+  },
+];
+
 function BuyerCompareQuotes() {
+  // set up the dispatch
+  const dispatch = useDispatch();
+
+  // retrieve the list of agencies that can offer the buyer's selection of features
+  // const selectedFeatures = useSelector((store = store.selectedFeatures));
+  const quotingAgencies = useSelector((store) => store.quotingAgencies);
+
+  // on page load
+  useEffect(() => {
+    // we need to send only the feature ids, so we need to filter for the payload
+    console.log(
+      `selectedFeature ids:`,
+      selectedFeatures.map((f) => f.id)
+    );
+    dispatch({
+      type: 'GET_QUOTING_AGENCIES',
+      payload: [6],
+    });
+  }, []);
+
+  console.log(`quotingAgencies:`, quotingAgencies);
   return (
     <>
       <Box sx={{ display: 'flex' }}>
