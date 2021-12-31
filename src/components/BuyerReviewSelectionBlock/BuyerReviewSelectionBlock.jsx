19551fd5e725bc793ffef38e70fa0536403fc435
Matt Dow <matt.dow.vo@gmail.com>
2021-12-31 17:41:52 -0600

    Added average cost functionality to buyer review data block, still has a bug

--- src/components/BuyerReviewSelectionBlock/BuyerReviewSelectionBlock.jsx
+++ src/components/BuyerReviewSelectionBlock/BuyerReviewSelectionBlock.jsx
@@ -12,15 +12,43 @@ function BuyerReviewSelectionBlock({ feature }) {
   const quoteData = useSelector((store) => store.agencyQuoteData);
   // grab selected features from the store. Only needed for the quantity
   const selectedFeatures = useSelector((store) => store.selectedFeatures);
-  // determine the quantity the client wants
+  // determine the quantity the client wants for the given feature
   const quantity = selectedFeatures.find(
     (f) => f.id === feature.id)
     .quantity;
+  // helper function to convert T-shirt size to quote field
+  const tShirtField = (shirtSize) => {
+    switch (shirtSize) {
+      case 'XS':
+        return 'xsmall_hours';
+      case 'S':
+        return 'small_hours';
+      case 'M':
+        return 'medium_hours';
+      case 'L':
+        return 'large_hours';
+      case 'XL':
+        return 'xlarge_hours';
+    }
+  };
+  // extract the quote data for the particular feature
+  const featureQuotes = quoteData.filter((d) => d.feature_id === feature.id);
+
+  // use this quote data to get an array of costs from each agency
+  const agencyCosts = featureQuotes.map((fq) => 
+  fq.hourly_rate * fq[tShirtField(fq.t_shirt_size)]);
+
+  // finally, use the reduce function to take the average of the 
+  // quotes
+  const avgCost = agencyCosts.reduce((a, b) => (a + b))/agencyCosts.length;
+
+  
+  
     return (
         <Box sx={{ display: 'flex' }}>
             <Box>
                 <Typography component="div" variant="body2">
-                    AVERAGE ESTIMATED COST: ${quantity * 100}
+                    AVERAGE ESTIMATED COST: ${quantity * avgCost}
                 </Typography>
                 <Typography component="div" variant="h5">
                     {feature.feature_name}
