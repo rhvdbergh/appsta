eaab99cd4e57e243c36763ec17b582dd333ec275
Ronald van der Bergh <rhvdbergh@gmail.com>
2022-01-03 23:30:39 -0600

    refactors buyercomparequotes to use latest saved project id from db, not reducer

--- src/components/BuyerCompareQuotes/BuyerCompareQuotes.jsx
+++ src/components/BuyerCompareQuotes/BuyerCompareQuotes.jsx
@@ -1,5 +1,5 @@
 import { Box } from '@mui/material';
-import { useEffect } from 'react';
+import { useEffect, useReducer } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import BuyerQuotesList from '../BuyerQuotesList/BuyerQuotesList';
 
@@ -7,15 +7,23 @@ function BuyerCompareQuotes() {
   // set up the dispatch
   const dispatch = useDispatch();
 
-  // retrieve the list of agencies that can offer the buyer's selection of features
+  // retrieve the user, list of agencies that can offer the buyer's selection of features
+  const user = useSelector((store) => store.user);
   const projectFeatures = useSelector((store) => store.projectFeatures);
   const quotingAgencies = useSelector((store) => store.quotingAgencies);
   const activeProject = useSelector((store) => store.activeProject);
 
-  // on page load, grab the projectFeatures for this buyer's project
+  // on page load, retrieve the latests saved project
+  // this should be the project just saved to the db
+  useEffect(() => {
+    dispatch({ type: 'GET_LATEST_PROJECT', payload: user.buyers_id });
+  });
+
+  // when we have a new active project,
+  // grab the projectFeatures for this buyer's project
   useEffect(() => {
     dispatch({ type: 'GET_PROJECT_FEATURES', payload: activeProject });
-  }, []);
+  }, [activeProject]);
 
   // when projectFeatures changes, update the quoting agencies
   useEffect(() => {
