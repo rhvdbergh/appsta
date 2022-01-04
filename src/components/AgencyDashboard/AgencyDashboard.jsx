691d6d52dbf638676398a51737bc50703fe5bfce
Hamsa Alimci <alimici2016@gmail.com>
2022-01-04 15:11:26 -0600

    Made a dialogue for the update on the dashboard

--- src/components/AgencyDashboard/AgencyDashboard.jsx
+++ src/components/AgencyDashboard/AgencyDashboard.jsx
@@ -9,6 +9,11 @@ import Checkbox from '@mui/material/Checkbox';
 import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import FormControl from '@mui/material/FormControl';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 
 function AgencyDashboard() {
   const user = useSelector((store) => store.user);
@@ -52,141 +57,184 @@ function AgencyDashboard() {
   const handleUpdate = () => {
     const agencyID = user.agency_id
     dispatch({ type: 'UPDATE_AGENCY_INFORMATION', payload: { agency, agencyID } })
+
   }
+  const [open, setOpen] = React.useState(false);
+  const [scroll, setScroll] = React.useState('paper');
+
+  const handleClickOpen = (scrollType) => () => {
+    setOpen(true);
+    setScroll(scrollType);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const descriptionElementRef = React.useRef(null);
+  React.useEffect(() => {
+    if (open) {
+      const { current: descriptionElement } = descriptionElementRef;
+      if (descriptionElement !== null) {
+        descriptionElement.focus();
+      }
+    }
+  }, [open]);
 
   return (
     <>
       <h1> AGENCY DASHBOARD </h1>
-      <p>
+      {/* <p>
         {' '}
         put some user data on this page to confirm that we are getting correct
         data..... placeholder{' '}
       </p>
-      <p> will need specific information for the agency.  </p>
+      <p> will need specific information for the agency.  </p> */}
       {/* <Navbar /> */}
 
       <div>
-        <Stack>
-          <TextField
-            label="Agency Name*"
-            value={agency_name}
-            variant="outlined"
-            onChange={(event) => setAgency_name(event.target.value)}
-          />
-          <TextField
-            label="Tell us more about your company"
-            multiline={true}
-            rows={3}
-            sx={{ width: 0.3, height: 100 }}
-            value={user.agency_blurb}
-            onChange={(event) => setAgency_blurb(event.target.value)}
-          />
-          <TextField label="Logo URL" variant="outlined"
-            value={user.logo_url}
-            onChange={(event) => setLogo_url(event.target.value)}
-          />
-          <TextField
-            label="Contact First Name*"
-            variant="outlined"
-            value={contact_first_name}
-            onChange={(event) => setContact_first_name(event.target.value)}
-          />
-          <TextField
-            label="Contact Last Name*"
-            variant="outlined"
-            value={contact_last_name}
-            onChange={(event) => setContact_last_name(event.target.value)}
-          />
-          <TextField
-            label="Phone Number"
-            variant="outlined"
-            type="tel"
-            value={phone_number}
-            onChange={(event) => setPhone_number(event.target.value)}
-          />
-        </Stack>
-        <Stack>
-          <TextField
-            label="Team Size (0-9999)*"
-            variant="outlined"
-            value={team_size}
-            onChange={(event) => setTeam_size(event.target.value)}
-          />
-
-          <FormControl component="fieldset">
-            <FormGroup row aria-label="minority-owned">
-              <FormControlLabel
-                control={<Checkbox />}
-                label="Minority Owned"
-                value={minority_owned}
-                checked={minority_owned}
-                onChange={(event) => setMinority_owned(event.target.checked)}
-              />
-            </FormGroup>
-
-            <FormGroup row aria-label="women-owned">
-              <FormControlLabel
-                control={<Checkbox />}
-                label="Women Owned"
-                value={woman_owned}
-                checked={woman_owned}
-                onChange={(event) => setWoman_owned(event.target.checked)}
-              />
-            </FormGroup>
-
-            <FormGroup row aria-label="veteran-owned">
-              <FormControlLabel
-                control={<Checkbox />}
-                label="Veteran Owned"
-                checked={veteran_owned}
-                value={veteran_owned}
-                onChange={(event) => setVeteran_owned(event.target.checked)}
-              />
-            </FormGroup>
-          </FormControl>
-          <Grid>
-            <TextField
-              label="City*"
-              value={city}
-              variant="outlined"
-              onChange={(event) => setCity(event.target.value)}
-              
-            />
-            <TextField
-              label="State/Province"
-              value={state_province}
-              variant="outlined"
-              onChange={(event) => setState_province(event.target.value)}
-             />
-            <TextField
-              label="Country Code"
-              value={country_code}
-              variant="outlined"
-              onChange={(event) => setCountry_code(event.target.value)}   
-            />
-            <TextField
-              label="Zip/Postal Code*"
-              value={postal_code}
-              variant="outlined"
-              onChange={(event) => setPostal_code(event.target.value)}  
-            />
-          </Grid>
-          <FormControl component="fieldset">
-            <FormLabel component="legend">Staff Location*</FormLabel>
-            <RadioGroup
-              aria-label="staffing-location"
-              name="radio-buttons-group"
-              value={staffing_location}
-              onChange={(event) => setStaffing_location(event.target.value)}
+        <Button onClick={handleClickOpen('body')}>scroll=body</Button>
+        <Dialog
+          open={open}
+          onClose={handleClose}
+          scroll={scroll}
+          aria-labelledby="scroll-dialog-title"
+          aria-describedby="scroll-dialog-description"
+        >
+          <DialogTitle id="scroll-dialog-title">Subscribe</DialogTitle>
+          <DialogContent dividers={scroll === 'paper'}>
+            <DialogContentText
+              id="scroll-dialog-description"
+              ref={descriptionElementRef}
+              tabIndex={-1}
             >
-              <FormControlLabel value={"Onshore Talent Only"} control={<Radio />} label="Onshore Talent Only" />
-              <FormControlLabel value="Onshore and Offshore Talent" control={<Radio />} label="Onshore and Offshore Talent" />
-              <FormControlLabel value="Offshore Talent, Onshore Leadership" control={<Radio />} label="Offshore Talent, Onshore Leadership" />
-              <FormControlLabel value="All Staff Offshore" control={<Radio />} label="All Staff Offshore" />
-            </RadioGroup>
-          </FormControl>
-        </Stack>
-        <Button onClick={handleUpdate}>Update</Button>
+              <Stack>
+                <TextField
+                  label="Agency Name*"
+                  value={agency_name}
+                  variant="outlined"
+                  onChange={(event) => setAgency_name(event.target.value)}
+                />
+                <TextField
+                  label="Tell us more about your company"
+                  multiline={true}
+                  rows={3}
+                  sx={{ width: 0.3, height: 100 }}
+                  value={user.agency_blurb}
+                  onChange={(event) => setAgency_blurb(event.target.value)}
+                />
+                <TextField label="Logo URL" variant="outlined"
+                  value={user.logo_url}
+                  onChange={(event) => setLogo_url(event.target.value)}
+                />
+                <TextField
+                  label="Contact First Name*"
+                  variant="outlined"
+                  value={contact_first_name}
+                  onChange={(event) => setContact_first_name(event.target.value)}
+                />
+                <TextField
+                  label="Contact Last Name*"
+                  variant="outlined"
+                  value={contact_last_name}
+                  onChange={(event) => setContact_last_name(event.target.value)}
+                />
+                <TextField
+                  label="Phone Number"
+                  variant="outlined"
+                  type="tel"
+                  value={phone_number}
+                  onChange={(event) => setPhone_number(event.target.value)}
+                />
+              </Stack>
+              <Stack>
+                <TextField
+                  label="Team Size (0-9999)*"
+                  variant="outlined"
+                  value={team_size}
+                  onChange={(event) => setTeam_size(event.target.value)}
+                />
+
+                <FormControl component="fieldset">
+                  <FormGroup row aria-label="minority-owned">
+                    <FormControlLabel
+                      control={<Checkbox />}
+                      label="Minority Owned"
+                      value={minority_owned}
+                      checked={minority_owned}
+                      onChange={(event) => setMinority_owned(event.target.checked)}
+                    />
+                  </FormGroup>
+
+                  <FormGroup row aria-label="women-owned">
+                    <FormControlLabel
+                      control={<Checkbox />}
+                      label="Women Owned"
+                      value={woman_owned}
+                      checked={woman_owned}
+                      onChange={(event) => setWoman_owned(event.target.checked)}
+                    />
+                  </FormGroup>
+
+                  <FormGroup row aria-label="veteran-owned">
+                    <FormControlLabel
+                      control={<Checkbox />}
+                      label="Veteran Owned"
+                      checked={veteran_owned}
+                      value={veteran_owned}
+                      onChange={(event) => setVeteran_owned(event.target.checked)}
+                    />
+                  </FormGroup>
+                </FormControl>
+                <Grid>
+                  <TextField
+                    label="City*"
+                    value={city}
+                    variant="outlined"
+                    onChange={(event) => setCity(event.target.value)}
+
+                  />
+                  <TextField
+                    label="State/Province"
+                    value={state_province}
+                    variant="outlined"
+                    onChange={(event) => setState_province(event.target.value)}
+                  />
+                  <TextField
+                    label="Country Code"
+                    value={country_code}
+                    variant="outlined"
+                    onChange={(event) => setCountry_code(event.target.value)}
+                  />
+                  <TextField
+                    label="Zip/Postal Code*"
+                    value={postal_code}
+                    variant="outlined"
+                    onChange={(event) => setPostal_code(event.target.value)}
+                  />
+                </Grid>
+                <FormControl component="fieldset">
+                  <FormLabel component="legend">Staff Location*</FormLabel>
+                  <RadioGroup
+                    aria-label="staffing-location"
+                    name="radio-buttons-group"
+                    value={staffing_location}
+                    onChange={(event) => setStaffing_location(event.target.value)}
+                  >
+                    <FormControlLabel value={"Onshore Talent Only"} control={<Radio />} label="Onshore Talent Only" />
+                    <FormControlLabel value="Onshore and Offshore Talent" control={<Radio />} label="Onshore and Offshore Talent" />
+                    <FormControlLabel value="Offshore Talent, Onshore Leadership" control={<Radio />} label="Offshore Talent, Onshore Leadership" />
+                    <FormControlLabel value="All Staff Offshore" control={<Radio />} label="All Staff Offshore" />
+                  </RadioGroup>
+                </FormControl>
+              </Stack>
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose}>Cancel</Button>
+            <Button onClick={handleUpdate}>Update</Button>
+          </DialogActions>
+        </Dialog>
       </div>
     </>
   );
