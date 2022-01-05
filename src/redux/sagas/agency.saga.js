617a9c1466efdf1b232614617640875d738e8e6a
Ronald van der Bergh <rhvdbergh@gmail.com>
2022-01-04 22:52:43 -0600

    after agency registration, agency is moved to agency options page

--- src/redux/sagas/agency.saga.js
+++ src/redux/sagas/agency.saga.js
@@ -6,6 +6,8 @@ function* postNewAgency(action) {
   console.log('received payload in postNewAgency', action.payload);
   try {
     yield axios.post('/api/agency/new', action.payload);
+    // automatically log a user in after registration
+    yield put({ type: 'LOGIN', payload: action.payload });
   } catch (error) {
     console.log('error in post new agency', error);
     yield put({ type: 'POST_AGENCY_ERROR' });
@@ -13,20 +15,25 @@ function* postNewAgency(action) {
 }
 
 function* updateAgencyInformation(action) {
-  console.log('this is updated information from dashboard', action.payload.agencyID)
+  console.log(
+    'this is updated information from dashboard',
+    action.payload.agencyID
+  );
   try {
-    yield axios.put(`api/agency/${action.payload.agencyID}`, action.payload.agency)
-    yield put({ type: 'FETCH_USER' })
+    yield axios.put(
+      `api/agency/${action.payload.agencyID}`,
+      action.payload.agency
+    );
+    yield put({ type: 'FETCH_USER' });
   } catch (error) {
-    console.log('error in agency information update', error)
-    yield put({ type: 'UPDATE_AGENCY_ERROR' })
+    console.log('error in agency information update', error);
+    yield put({ type: 'UPDATE_AGENCY_ERROR' });
   }
 }
 
-
 function* agencySaga() {
   yield takeLatest('ADD_NEW_AGENCY', postNewAgency);
-  yield takeLatest('UPDATE_AGENCY_INFORMATION', updateAgencyInformation)
+  yield takeLatest('UPDATE_AGENCY_INFORMATION', updateAgencyInformation);
 }
 
 export default agencySaga;
