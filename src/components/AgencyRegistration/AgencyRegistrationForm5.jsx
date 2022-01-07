da8c137da2e3e762209e9828b5f2aa609027ff17
Ronald van der Bergh <rhvdbergh@gmail.com>
2022-01-06 22:30:15 -0600

    adds estimate data / conversion settings as step 5, updates newAgency reducer, submit button correctly renders

--- src/components/AgencyRegistration/AgencyRegistrationForm5.jsx
+++ src/components/AgencyRegistration/AgencyRegistrationForm5.jsx
@@ -1,5 +1,109 @@
+import { Box, Typography, TextField, Button } from '@mui/material';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+
 function AgencyRegistrationForm5({ setCanMoveForward }) {
-  return <p>Form 5</p>;
+  // set up the redux dispatch
+  const dispatch = useDispatch();
+
+  // fetch the new agency object being built in the redux store
+  const agency = useSelector((store) => store.newAgency);
+
+  // handle changes to the object
+  const handleChange = (data, value) => {
+    dispatch({
+      type: 'SET_NEW_AGENCY',
+      payload: { ...agency, [data]: parseInt(value) },
+    });
+  };
+
+  // test to see if form is completed every time agency changes
+  useEffect(() => {
+    isCompletedForm();
+  }, [agency]);
+
+  const isCompletedForm = () => {
+    if (
+      agency.xsmall_hours &&
+      agency.small_hours &&
+      agency.medium_hours &&
+      agency.large_hours &&
+      agency.xlarge_hours &&
+      agency.hourly_rate
+    ) {
+      setCanMoveForward(true);
+    } else {
+      setCanMoveForward(false);
+    }
+  };
+
+  return (
+    <Box sx={{ my: 2 }}>
+      <Typography type="h6" sx={{ my: 2 }}>
+        Select Your Estimate Data
+      </Typography>
+      <TextField
+        sx={{ m: 1 }}
+        label="Hours for XS T-Shirt Size"
+        variant="outlined"
+        type="number"
+        value={agency.xsmall_hours || ''}
+        onChange={(event) => {
+          handleChange('xsmall_hours', event.target.value);
+        }}
+      />
+      <TextField
+        sx={{ m: 1 }}
+        label="Hours for S T-Shirt Size"
+        variant="outlined"
+        type="number"
+        value={agency.small_hours || ''}
+        onChange={(event) => {
+          handleChange('small_hours', event.target.value);
+        }}
+      />
+      <TextField
+        sx={{ m: 1 }}
+        label="Hours for M T-Shirt Size"
+        variant="outlined"
+        type="number"
+        value={agency.medium_hours || ''}
+        onChange={(event) => {
+          handleChange('medium_hours', event.target.value);
+        }}
+      />
+      <TextField
+        sx={{ m: 1 }}
+        label="Hours for L T-Shirt Size"
+        variant="outlined"
+        type="number"
+        value={agency.large_hours || ''}
+        onChange={(event) => {
+          handleChange('large_hours', event.target.value);
+        }}
+      />
+      <TextField
+        sx={{ m: 1 }}
+        label="Hours for XL T-Shirt Size"
+        variant="outlined"
+        type="number"
+        value={agency.xlarge_hours || ''}
+        onChange={(event) => {
+          handleChange('xlarge_hours', event.target.value);
+        }}
+      />
+      <TextField
+        sx={{ m: 1 }}
+        label="Hourly Rate"
+        variant="outlined"
+        type="number"
+        value={agency.hourly_rate || ''}
+        onChange={(event) => {
+          handleChange('hourly_rate', event.target.value);
+        }}
+      />
+    </Box>
+  );
 }
 
 export default AgencyRegistrationForm5;
