1d4f64cfd1c216f2f57869c998e9e0c7c3111a37
Ronald van der Bergh <rhvdbergh@gmail.com>
2022-01-08 07:28:52 -0600

    db-backup retrieves table names

--- server/modules/db-backup.js
+++ server/modules/db-backup.js
@@ -1,5 +1,24 @@
+const pool = require('../modules/pool');
+
 const dbBackup = (server) => {
+  // this query returns all the public tables in the db
+  const query = `
+    SELECT table_name 
+    FROM information_schema.tables
+    WHERE table_schema = 'public';
+  `;
   console.log('in dbBackup');
+  pool
+    .query(query)
+    .then((response) => {
+      console.log(response.rows);
+    })
+    .catch((err) =>
+      console.log(
+        'there was an error while retrieving the tables in the database:',
+        err
+      )
+    );
   console.log('Backup complete: closing server...');
   server.close();
 };
