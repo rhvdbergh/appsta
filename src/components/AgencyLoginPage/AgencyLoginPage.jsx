a9b379e18ae71b059674f4fe15cdd27241d27c1c
Matt Dow <matt.dow.vo@gmail.com>
2022-01-09 14:21:46 -0600

    Adjusted Landing Page and Agency Login page

--- src/components/AgencyLoginPage/AgencyLoginPage.jsx
+++ src/components/AgencyLoginPage/AgencyLoginPage.jsx
@@ -1,15 +1,15 @@
-import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
-import { Box, Button, Typography } from '@mui/material';
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { Box, Button, Typography } from "@mui/material";
 
 // CUSTOM COMPONENTS
-import LoginForm from '../LoginForm/LoginForm';
+import LoginForm from "../LoginForm/LoginForm";
 
 function AgencyLoginPage() {
   const history = useHistory();
 
   const onLogin = (event) => {
-    history.push('/login');
+    history.push("/login");
   };
 
   return (
@@ -17,29 +17,26 @@ function AgencyLoginPage() {
       <center>
         <LoginForm isAgency="true" />
         <Box>
-          <Typography
-          variant="body"
-        > First time here?
-        </Typography>
-          <Button 
-            color="secondary"
+          <Typography variant="h6"> First time agency?</Typography>
+          <Button
+            sx = {{ color: "primary.dark" }}
             onClick={() => {
-            history.push('/AgencyReg');
-          }}>
-             Register Now </Button>
+              history.push("/AgencyReg");
+            }}
+          >
+            <Typography variant="h6">REGISTER NOW</Typography>
+          </Button>
         </Box>
         <Button
-          color='secondary'
+          color="secondary"
           variant="contained"
-          onClick= {() => {
-              history.push('/LandingPage');
-          }}>
-          Not an Agency? Click Here
-          </Button>
-
-
-        
-        
+          sx={{ m: 3 }}
+          onClick={() => {
+            history.push("/LandingPage");
+          }}
+        >
+          <Typography variant="h6" sx={{ color: "white" }}>Not an Agency? Click Here</Typography>
+        </Button>
       </center>
     </Box>
   );
