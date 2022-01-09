54158ef34d093980e50d37eae74c2c3316720448
Ronald van der Bergh <rhvdbergh@gmail.com>
2022-01-09 13:28:20 -0600

    adds detail from feature-demo-features

--- database.sql
+++ database.sql
@@ -479,12 +479,12 @@ VALUES
 INSERT INTO
   feature_categories ("category_name")
 VALUES
-  ('charts'),
-  ('buttons'),
+  ('authenication'),
+  ('subscription'),
   ('tables'),
   ('calendars'),
-  ('icons'),
-  ('input fields');
+  ('menus'),
+  ('forms');
 
   INSERT INTO
   features (
@@ -496,46 +496,60 @@ VALUES
   )
 VALUES
   (
-    'doughnut chart',
-    'information in a doughnut chart',
-    'description of object',
+    'login',
+    'username and password',
+    'description of login',
     '1',
-    'https://pnp.github.io/sp-dev-fx-controls-react/assets/DoughnutChart.png'
+    'https://miro.medium.com/max/1400/1*_tt-dyUruSDMokKv-kQz9Q.png'
   ),
   (
-    'bar chart',
-    'information in a bar chart',
-    'description of object',
+    'register',
+    'register new user',
+    'description of registration',
     '1',
-    'https://pnp.github.io/sp-dev-fx-controls-react/assets/BarChart.png'
+    'https://www.tutorialrepublic.com/snippets/designs/simple-registration-form.png'
   ),
   (
-    'big red button',
-    'I can click on big red button',
-    'description of clicking on big red button',
+    'password reset',
+    'user can reset lost password ',
+    'description of password reset',
+    '1',
+    'https://shots.jotform.com/welvin/shots/2019-11-22-012.png?1.0.449'
+  ),
+  (
+    'pop up',
+    'pop up subscription form appears when user loads page ',
+    'description of pop up',
     '2',
-    'https://www.royalroadcdn.com/public/covers-full/big-red-button-aacalrkedw8.jpg?time=1617332304'
+    'https://www.mailerlite.com/assets/features/popup-subscribe-form.jpeg'
   ),
   (
-    'outlined button',
-    'I can click the outlined button',
-    'description of clicking on outlined button',
+    'header',
+    'subscription form in header',
+    'description of header ',
     '2',
-    'https://storage.googleapis.com/spec-host-backup/mio-components%2Fassets%2F1b2CXpWNxh1k2YJUw_6B_CkkZUiHYYrzh%2Fspecs-outlined-button.png'
+    'https://assets.getsitecontrol.com/prod2/blog/lead-generation/7-great-email-newsletter-signup-examples/12.png?rev=e2143a474c'
   ),
   (
-    'colored table',
+    'footer',
+    'subscription form in footer',
+    'description of footer',
+    '2',
+    'https://help.outofthesandbox.com/hc/article_attachments/360032537374/footer-newsletter-parallax.png'
+  ),
+  (
+    'bootstrap table',
     'I can create a simple table',
     'colored table',
     '3',
     'https://reactjsexample.com/content/images/2019/01/material-table.jpg'
   ),
   (
-    'clear table',
-    'this table is clear',
-    'clear table',
+    'data table',
+    'this is a data table ',
+    'data table',
     '3',
-    'https://i.stack.imgur.com/jm4zL.png'
+    'https://s3-alpha.figma.com/hub/file/217425317/401965ec-d8b1-4315-b722-beefad4aa135-cover'
   ),
   (
     'calendars',
@@ -552,32 +566,32 @@ VALUES
     'https://camo.githubusercontent.com/d3b80e150c29d8530b289054210aa68f6398cdba8f62fb2feac4ef15b94c9cc4/68747470733a2f2f73746f726167652e676f6f676c65617069732e636f6d2f6d6174657269616c2d64657369676e2f7075626c6973682f6d6174657269616c5f765f31322f6173736574732f304233333231735a4c6f505f484f545a6f55453156534778765246452f636f6d706f6e656e74732d7069636b6572732d64617465342e706e67'
   ),
   (
-    'icons',
-    'this is an icon list',
-    'description of object',
+    'top navigation ',
+    'this is header navigation',
+    'description of header nav ',
     '5',
-    'https://miro.medium.com/max/1400/1*2QQNpKIBeyTha-XcU9IHBw.png'
+    'https://cdn.dribbble.com/users/1774675/screenshots/15763538/media/76834f405a810f7d28ad39f4a26083a9.png?compress=1&resize=400x300'
   ),
   (
-    'icons',
-    'this is another icon list',
-    'description of object',
+    'bottom navigation',
+    'this is footer navigation',
+    'description of footer navigation',
     '5',
-    'https://icons.iconarchive.com/icons/custom-icon-design/mini/icons-390.jpg'
+    'https://camo.githubusercontent.com/1a6072b665137d805c35cdc52f079b2ce432fdb9fcfb40667ead0b48f048b1b2/687474703a2f2f692e696d6775722e636f6d2f5959374b4a73532e706e67'
   ),
   (
-    'input fields',
-    'here are some text fields',
-    'descirption of text fields',
+    'contact',
+    'contact form description',
+    'descirption of contact form',
     '6',
-    'https://i.pinimg.com/originals/61/6b/5e/616b5e964eb01bbcb554306ac6744326.png'
+    'https://bootstrapstudio.io/docs/img/guide/contact-form-example.png'
   ),
   (
-    'input fields',
-    'here are some more text fields',
-    'descirption of text fields',
+    'intake',
+    'client intake form',
+    'descirption of intake form',
     '6',
-    'https://lh3.googleusercontent.com/Cg9IlC6OWQ58eqWlbkPacWsO6vemJOcOzkRHsF7nnAVa6VD-GZgcMDGfPpHxcpS4FbhEqWiUO-W48Pd18FfR-cYrdPTo1bWjEIti=w1064-v0'
+    'https://d2ydtwisqcxv72.cloudfront.net/client-intake-form.png'
   );
 
   INSERT INTO
