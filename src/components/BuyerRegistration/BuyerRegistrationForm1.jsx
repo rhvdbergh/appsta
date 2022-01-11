a8df988c83aa090b34a951d64ec3ca27fdfea2d5
Ronald van der Bergh <rhvdbergh@gmail.com>
2022-01-11 14:44:40 -0600

    cleans buyerregf1

--- src/components/BuyerRegistration/BuyerRegistrationForm1.jsx
+++ src/components/BuyerRegistration/BuyerRegistrationForm1.jsx
@@ -7,50 +7,61 @@ import {
   InputAdornment,
   IconButton,
   Box,
-} from "@mui/material";
-import Visibility from "@mui/icons-material/Visibility";
-import VisibilityOff from "@mui/icons-material/VisibilityOff";
-import Grid from "@mui/material/Grid";
+  Grid,
+} from '@mui/material';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
 //import from react
-import { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
-import { useDispatch } from "react-redux";
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
+// this component displays on the BuyerRegistration main view
+// setCanMoveForward requires validation to move to the next form
 function BuyerRegistrationForm1({ setCanMoveForward }) {
+  // set up the redux dispatch
   const dispatch = useDispatch();
 
+  // retrieve the buyer object from the redux store
+  // this is the buyer object that will be submitted to the database
+  // each successive "step" on the BuyerRegistration page fills
+  // out more details of this object
   const buyer = useSelector((store) => store.newBuyer);
 
+  // define local state to capture user input
+
   const [username, setUsername] = useState(null);
-
   const [password, setPassword] = useState(null);
-
   const [passwordShown, setPasswordShown] = useState(false);
   const [passwordConfirmationShown, setPasswordConfirmationShown] =
     useState(false);
-
   const [passwordConfirmation, setPasswordConfirmation] = useState(null);
 
+  // on page load, set the local state to the entries coming
+  // from the reducer
+  // should the user step back, the entries in the reducer
+  // repopulates with what the user entered previously
   useEffect(() => {
     setUsername(buyer.username);
     setPassword(buyer.password);
     setPasswordConfirmation(buyer.password);
-    // isCompletedForm();
   }, []);
 
+  // if the local state changes, the reducers have been updated
+  // so run the validation to see if all the required fields
+  // have been updated
   useEffect(() => {
     isCompletedForm();
   }, [username, password, passwordConfirmation]);
 
   // check to see if everything is filled out and
   // the user can move forward
-  // black list
   const isCompletedForm = () => {
     if (
       username !== null &&
       password !== null &&
-      password !== "" &&
+      password !== '' &&
       passwordConfirmation !== null &&
       password === passwordConfirmation
     ) {
@@ -60,61 +71,64 @@ function BuyerRegistrationForm1({ setCanMoveForward }) {
     }
   };
 
+  // data expects a string with the property to set on the newBuyer reducer
   const handleData = (data, value) => {
     // validate the user password
     // both password fields should have the same content and not be empty
-    if (data === "password" || data === "passwordConfirmation") {
-      if (password === passwordConfirmation && password !== "") {
+    if (data === 'password' || data === 'passwordConfirmation') {
+      if (password === passwordConfirmation && password !== '') {
         dispatch({
-          type: "SET_NEW_BUYER",
+          type: 'SET_NEW_BUYER',
           payload: { ...buyer, password: value },
         });
       } // end if check equal passwords
     } // end if data === password
-    else if (data !== "") {
+    else if (data !== '') {
       // check to see that the data field is not empty
       dispatch({
-        type: "SET_NEW_BUYER",
+        type: 'SET_NEW_BUYER',
         payload: { ...buyer, [data]: value },
       });
     }
+    // check if the user can move forward
     isCompletedForm();
   };
 
-  console.log("this is buyer", buyer);
   return (
     <Grid
       sx={{
-        display: "flex",
-        flexDirection: "column",
-        alignContent: "center",
+        display: 'flex',
+        flexDirection: 'column',
+        alignContent: 'center',
       }}
     >
-      <Box sx={{ display: "flex", justifyContent: "center" }}>
+      <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+        {/* onBlur captures the information as soon as the user moves */}
+        {/* out of the field */}
         <TextField
-          sx={{ width: "60%", maxWidth: "350px", my: 1.5 }}
+          sx={{ width: '60%', maxWidth: '350px', my: 1.5 }}
           label="Email*"
           variant="outlined"
           value={username}
           onChange={(event) => setUsername(event.target.value)}
           onBlur={() => {
-            handleData("username", username);
+            handleData('username', username);
           }}
         />
       </Box>
-      <Box sx={{ display: "flex", justifyContent: "center" }}>
+      <Box sx={{ display: 'flex', justifyContent: 'center' }}>
         <FormControl
           variant="filled"
-          sx={{ width: "60%", maxWidth: "350px", my: 1.5 }}
+          sx={{ width: '60%', maxWidth: '350px', my: 1.5 }}
         >
           <InputLabel htmlFor="filled-adornment-password">Password</InputLabel>
           <OutlinedInput
             id="filled-adornment-password"
-            type={passwordShown ? "text" : "password"}
+            type={passwordShown ? 'text' : 'password'}
             value={password}
             onChange={(event) => setPassword(event.target.value)}
             onBlur={() => {
-              handleData("password", password);
+              handleData('password', password);
             }}
             endAdornment={
               <InputAdornment position="end">
@@ -130,21 +144,21 @@ function BuyerRegistrationForm1({ setCanMoveForward }) {
           />
         </FormControl>
       </Box>
-      <Box sx={{ display: "flex", justifyContent: "center" }}>
+      <Box sx={{ display: 'flex', justifyContent: 'center' }}>
         <FormControl
           variant="filled"
-          sx={{ width: "60%", maxWidth: "350px", my: 1.5 }}
+          sx={{ width: '60%', maxWidth: '350px', my: 1.5 }}
         >
           <InputLabel htmlFor="filled-adornment-password">
             Confirm Password
           </InputLabel>
           <OutlinedInput
             id="filled-adornment-password"
-            type={passwordConfirmationShown ? "text" : "password"}
+            type={passwordConfirmationShown ? 'text' : 'password'}
             value={passwordConfirmation}
             onChange={(event) => setPasswordConfirmation(event.target.value)}
             onBlur={() => {
-              handleData("passwordConfirmation", passwordConfirmation);
+              handleData('passwordConfirmation', passwordConfirmation);
             }}
             endAdornment={
               <InputAdornment position="end">
