991360c3b3ed89c3eabfb1bbf63b4ad06b57d4fa
Ronald van der Bergh <rhvdbergh@gmail.com>
2022-01-11 19:59:06 -0600

    cleans reducers

--- src/redux/reducers/buyer.reducer.js
+++ src/redux/reducers/buyer.reducer.js
@@ -2,27 +2,25 @@
 // for a new agency user to register
 
 const emptyNewBuyer = {
-    username: null,
-    password: null,
-    company_name: null,
-    project_name: null,
-    first_name: null,
-    last_name: null,
-    city: null,
-    //took out country code - wasn't in db 
-    postal_code: null,
-  };
-  
-  const newBuyerReducer = (state = emptyNewBuyer, action) => {
-    switch (action.type) {
-      case 'SET_NEW_BUYER':
-        return action.payload;
-      case 'LOGOUT':
-        return emptyNewBuyer;
-      default:
-        return state;
-    }
-  };
-  
-  export default newBuyerReducer;
-  
+  username: null,
+  password: null,
+  company_name: null,
+  project_name: null,
+  first_name: null,
+  last_name: null,
+  city: null,
+  postal_code: null,
+};
+
+const newBuyerReducer = (state = emptyNewBuyer, action) => {
+  switch (action.type) {
+    case 'SET_NEW_BUYER':
+      return action.payload;
+    case 'LOGOUT':
+      return emptyNewBuyer;
+    default:
+      return state;
+  }
+};
+
+export default newBuyerReducer;
--- src/redux/reducers/feature.reducer.js
+++ src/redux/reducers/feature.reducer.js
@@ -1,13 +1,10 @@
-
-
-
 const featureReducer = (state = [], action) => {
-    switch (action.type) {
-        case 'SET_FEATURES':
-            return action.payload;
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case 'SET_FEATURES':
+      return action.payload;
+    default:
+      return state;
+  }
+};
 
 export default featureReducer;
